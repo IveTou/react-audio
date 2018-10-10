af8c6452fe87e8b37b4c651983cf079f1c0a614f
Ive <ive.tourinho@itmov.com.br>
2018-10-10 10:09:27 -0300

    Basic of

--- src/components/audio-visualizer/index.js
+++ src/components/audio-visualizer/index.js
@@ -4,25 +4,55 @@ import autoBind from 'react-autobind';
 
 import { withIndexStyle } from './styles';
 
+//https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Visualizations_with_Web_Audio_API
+//https://css-tricks.com/introduction-web-audio-api/
+//https://www.creativebloq.com/netmag/how-manipulate-and-visualise-web-audio-91413066
+//http://ianreah.com/2013/02/28/Real-time-analysis-of-streaming-audio-data-with-Web-Audio-API.html
+//
+
 export class AudioVisualizer extends React.Component {
   constructor(props) {
     super(props);
     autoBind(this);
-    this.analyser = new (window.AudioContext || window.webkitAudioContext)().createAnalyser();
+
+    this.context = new (window.AudioContext || window.webkitAudioContext)();
+    this.source = this.context.createOscillator();
+    this.gain = this.context.createGain();
+    this.analyser = this.context.createAnalyser();
+
+    //configure source
+    this.source.type = 'sine';
+    this.source.frequency.value = 90;
+
+    //connect source to gain and gain to destination 
+    this.source.connect(this.gain);
+    this.gain.connect(this.context.destination);
+
+    //configure analyzer
     this.analyser.fftSize = 2048;
+
+    //connect source to analyser
+    this.source.connect(this.analyser);
+
+    //set data buffer
     const bufferLength = this.analyser.frequencyBinCount;
     this.dataArray = new Uint8Array(bufferLength);
   }
 
   componentDidMount() {
+    //start source
+    this.source.start();
+  }
+
+  handleVolume(value) {
+    const now = this.context.currentTime;
+    this.gain.gain.setValueAtTime(value, now);
   }
 
   renderWaves() {
-    const drawVisual = requestAnimationFrame(this.renderWaves);
+    requestAnimationFrame(this.renderWaves);
     this.analyser.getByteTimeDomainData(this.dataArray);
-    //Write draw function here
-    console.log(this.dataArray);
-    //console.log(drawVisual);
+    console.log(this.dataArray)
   }
 
   render() {
